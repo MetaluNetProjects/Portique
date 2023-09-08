8aebd263f18029261698c736a265ca04023891d1
Antoine Rousseau <_antoine_@metalu.net>
2023-09-08 16:24:08 +0000

    start working on OSC remote control

---
+++ OSCremote/runOSC.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd `dirname $0`
+#open-stage-control  -s localhost:18080 -t ./theme-contrast.css -l ./teleco-OSC.json
+open-stage-control -s localhost:18080 -l ./teleco-OSC.json --client-options fullscreen=1
+# --read-only
